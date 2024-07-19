1dbfb0c805fbdf6d124ce8aabd58efa3eb302578
web coders <webcoders1122@gmail.com>
2024-07-19 09:57:12 +0500

    blog completed without ssg

---
+++ blog-posts/src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import getPostsData from "../../lib/getPostsData";
+import { Metadata } from "next";
+import PostListing from "./components/PostListing";
+
+export const metadata: Metadata = {
+  title: "Welcome to MyBlog",
+};
+export default function Home() {
+  const postData: blogPost[] = getPostsData();
+
+  return (
+    <main className='bg-slate-700 prose prose-xl text-white mx-auto'>
+      {postData.map((post: blogPost) => {
+        return (
+          <PostListing
+            key={post.id}
+            post={post}
+          />
+        );
+      })}
+    </main>
+  );
+}
